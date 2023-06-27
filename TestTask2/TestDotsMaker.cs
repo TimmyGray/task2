fa1402b2302e9308052a2545b50748c8f5c90247
Timofey <84410960+TimmyGray@users.noreply.github.com>
2023-06-27 22:08:30 +0300

    simple unit tests

---
+++ TestTask2/TestDotsMaker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task2;
+
+namespace TestTask2
+{
+    [TestClass]
+    public class TestDotsMaker
+    {
+
+        [TestMethod]
+        public void TestDots()
+        {
+            string[] testdata = { 
+                "rest", "verylongtestdata",
+                "testdata2", "testdata1", "datatestd1" };
+
+
+            DotsMaker dotsMaker = new DotsMaker(16, 10, 1);
+            List<string> actualdata = new List<string>();
+            string[] expecteddata = {"..........",".",".....","..."};
+            int count = 1;
+            foreach (string test in testdata)
+            {
+                string dots = dotsMaker.MakeDots(count);
+                Console.WriteLine(count);
+                Console.WriteLine(dots);
+                actualdata.Add(dots);
+                count*= 2;
+            }
+
+            Assert.AreEqual(expecteddata[0].Length, actualdata[4].Length);
+            Assert.AreEqual(expecteddata[1].Length, actualdata[0].Length);
+            Assert.AreEqual(expecteddata[1].Length, actualdata[1].Length);
+            Assert.AreEqual(expecteddata[2].Length, actualdata[3].Length);
+            Assert.AreEqual(expecteddata[3].Length, actualdata[2].Length);
+
+        }
+
+    }
+}
---
+++ TestTask2/TestFinder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task2;
+
+namespace TestTask2
+{
+    [TestClass]
+    public class TestFinder
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+
+            string[] testdata = { "rest", "verylongtestdata", "testdata2", "testdata1", "rest", "verylongtestdata", "datatestd2", "datatestd1", "rest", "rest" };
+            Finder testf = new Finder(testdata);
+            Dictionary<string, int> expected = new Dictionary<string, int>
+            {
+                { "datatestd1", 1 },
+                { "datatestd2", 1 },
+                { "testdata1", 1 },
+                { "testdata2", 1 },
+                { "verylongtestdata", 2 },
+                { "rest", 4 }
+            };
+
+            foreach(var val in testf.Uniqwords)
+            {
+                Console.WriteLine(val.Key);
+            }
+
+            Assert.AreEqual(expected["datatestd1"], testf.Uniqwords.ElementAt(0).Value, $"Incorrect sorting - expected {expected["datatestd1"]}, actual - {testf.Uniqwords.ElementAt(0).Value}");
+            Assert.AreEqual(expected["datatestd2"], testf.Uniqwords.ElementAt(1).Value, $"Incorrect sorting - expected {expected["datatestd1"]}, actual - {testf.Uniqwords.ElementAt(1).Value}");
+            Assert.AreEqual(expected["rest"], testf.Uniqwords.ElementAt(5).Value, $"Incorrect sorting - expected {expected["rest"]}, actual - {testf.Uniqwords.ElementAt(5).Value}");
+            Assert.AreEqual(expected.ElementAt(4).Key, testf.longestword,$"Wrong longest word - expected {expected.ElementAt(4).Key}, actual - {testf.longestword}");
+
+        }
+    }
+}
